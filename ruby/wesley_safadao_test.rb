e51f35969a0d974be330dd5ec7af979772e355e7
Philippe Hardardt <phil.henri@gmail.com>
2018-01-28 19:29:49 -0200

    Use Minitest to verify WesleySafadao behaviour.

--- ruby/wesley_safadao_test.rb
+++ ruby/wesley_safadao_test.rb
@@ -1,3 +1,10 @@
 require_relative 'wesley_safadao.rb'
-puts WesleySafadao.new(4, 8, 94).say_safadeza
+require 'minitest/autorun'
 
+class WesleySafadaoTest < Minitest::Test
+  def test_that_it_prints_safadeza_summary
+    assert_output("Você é 79.24% safado e 20.76% anjo\n") do
+      WesleySafadao.new(4, 8, 94).say_safadeza
+    end
+  end
+end
